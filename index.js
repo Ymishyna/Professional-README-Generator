184c40eff0500b4115bb73ecd684c7d9600ba589
Yana Mishyna <yana.mishina.92@gmail.com>
2023-08-29 14:48:56 -0400

    finished questions to be asked for creating readme in index.js, finished adjusting outline of readme in generateMarkdown function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require('fs');
 const path = require('path');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -9,6 +10,54 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'What is your project name?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running (Optional).',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use. Include screenshots as needed.',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Choose a license for your project.',
+        choices: [
+            { value: 'MIT' }, //The MIT License
+            { value: 'Apache2.0' }, //Apache 2.0 License
+            { value: 'BSD3' }, //BSD 3-Clause License
+            { value: 'LGPL' }, //GNU LGPL v3
+            { value: 'MPL' }, //Mozilla Public License 2.0
+            { value: 'None' }, 
+        ]
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Explain how other developers can contribute to your application (Optional).',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Write tests for application, and provide examples on how to run them here (Optional).',
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username: ',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email: ',
+        name: 'email',
     }
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`;
+  }
+  return '';
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
+## Description 📝
+
+${data.description}
+
+## Table of Contents (Optional) 🗒
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation-💻)
+- [Usage](#usage-🏆)
+${renderLicenseLink(data.license)}
+- [Contributing](#contributing-😃)
+- [Test](#Test-🧪)
+- [Questions](#questions)
+
+## Installation 💻
+
+${data.installation}
+
+## Usage  🏆
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing  😃
+ 
+${data.contributing}
+
+## Tests  🧪
+
+${data.test}
+
+## Questions
+
+For additional questions, contact me at the email provided below. 
+
+- Email:  ${data.email}
+- GitHub: [${data.github}](https://github.com/${data.github}/)
+
+---
+
+_This README was generated with ❤️ by [README-Generator](https://github.com/Ymishyna/Professional-README-Generator) © 2023_
 `;
 }
 
